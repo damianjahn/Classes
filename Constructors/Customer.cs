7bc0f9be20b28a4f0a0cea685a7522a07c68685e
Damian Jahn <damian.jahn@icloud.com>
2018-10-23 22:11:26 +0200

    Classes

---
+++ Constructors/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        public int ID;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public Customer(int ID)
+            : this()
+        {
+            this.ID = ID;
+        }
+        public Customer(int ID, string name)
+            : this(ID)
+        {
+            this.ID = ID;
+            this.Name = name;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.ID);
+            Console.WriteLine(customer.Name);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
